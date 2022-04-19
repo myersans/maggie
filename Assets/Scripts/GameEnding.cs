2078aa91ab3465b15133bf3928da990531b247f3
myersans <89566183+myersans@users.noreply.github.com>
2022-04-19 13:59:50 -0400

    added in key  count

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -10,11 +10,16 @@ public class GameEnding : MonoBehaviour
     public float timeRemaining = 10;
     public bool timerIsRunning = false;
     public TextMeshProUGUI timeText;
+
+    private int keyCount;
+
     // Start is called before the first frame update
     void Start()
     {
         timerIsRunning = true;
         DisplayTime(timeRemaining);
+
+        keyCount = 3;
     }
 
     // Update is called once per frame
@@ -37,5 +42,33 @@ public class GameEnding : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         timeText.text = "Time Remaining: " + string.Format("{0:00}:{1:00}", minutes, seconds);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch(other.gameObject.tag)
+        {
+            case "Key":
+                other.gameObject.SetActive(false);
+                keyCount = keyCount - 1;
+
+            case "Door1":
+                if (keyCount = 2)
+                {
+                    other.gameObject.SetActive(false);
+                }
+
+            case "Door2":
+                if (keyCount = 1)
+                {
+                    other.gameObject.SetActive(false);
+                }
+
+            case "Door3":
+                if (keyCount = 0)
+                {
+                    other.gameObject.SetActive(false);
+                }
+        }
     }
 }
