17c6aff1e3cf515d09d2c493454ec23ae1838245
myersans <89566183+myersans@users.noreply.github.com>
2022-04-21 21:17:02 -0400

    added hag image

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -52,7 +52,7 @@ public class GameEnding : MonoBehaviour
 
     void SetClue1Text()
     {
-        clue1Text.text = "Free Bird     Over Yonder     Rejoice in your Flight     The wind, your palace";
+        clue1Text.text = "Free Bird Over Yonder Rejoice in your flight The wind, your palace";
     }
 
     void SetClue2Text()
